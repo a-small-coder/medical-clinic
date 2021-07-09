519613da46395fe5f16de07c1e0d8a93e2fc6cc5
oleg <hulktv222@mail.ru>
2021-07-09 20:17:41 +0300

    markup main blocks

---
+++ #src/js/files/script.js
@@ -0,0 +1,26 @@
+window.onload = function (){
+    document.addEventListener("click", documentActions);
+
+    // Actions (delegate click event)
+    function documentActions(e){
+        const targetElement = e.target;
+        if (window.innerWidth > 768 && isMobile.any()){
+            if (targetElement.classList.contains('menu__arrow')){
+                if (document.querySelectorAll('.menu__item._hover').length > 0){
+                    _removeClasses(document.querySelectorAll('.menu__item._hover'), "_hover");
+                }
+                targetElement.closest('.menu__item').classList.toggle('_hover');
+            }
+            if (!targetElement.closest('.menu__item') && document.querySelectorAll('.menu__item._hover').length > 0){
+                _removeClasses(document.querySelectorAll('.menu__item._hover'), "_hover");
+            }
+        }
+
+        if (targetElement.classList.contains('search-form__icon')){
+            document.querySelector('.search-form').classList.toggle('_active');
+        }
+        else if (!targetElement.closest('.search-form') && document.querySelectorAll('.search-form._active').length > 0){
+            _removeClasses(document.querySelectorAll('.search-form._active'), "_active");
+        }
+    }
+}
