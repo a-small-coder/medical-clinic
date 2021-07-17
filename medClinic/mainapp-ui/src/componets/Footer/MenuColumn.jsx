0d737d4ce7ad1b35c165361765e97ddb5c099fd8
oleg <hulktv222@mail.ru>
2021-07-17 16:00:43 +0300

    footer spoilers fix

--- medClinic/mainapp-ui/src/componets/Footer/MenuColumn.jsx
+++ medClinic/mainapp-ui/src/componets/Footer/MenuColumn.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 import { Link } from 'react-router-dom';
+import {slideUp, slideDown} from '../Spoiler';
 const MenuColumn = () => {
+
+    const [spoilerActive, setSpoilerActive] = useState(false);
+    const buttonClassName = "menu-footer__title _footer-title";
+    const contentClassName = "menu-footer__list";
+
+    const contentRef = useRef(null);    
+const _slideDown = useCallback(slideDown, []);
+const _slideUp = useCallback(slideUp, []);
+
+useEffect(() => {
+    if (spoilerActive){
+        _slideDown(contentRef);
+    }
+    else{
+        _slideUp(contentRef);
+    }
+}, [spoilerActive]);
+
     return (
-        <div class="menu-footer__column">
-            <button data-spoller type="button" class="menu-footer__title _footer-title">Menu</button>
-            <ul class="menu-footer__list">
-                <li><Link to="" class="menu-footer__link" activeClassName="menu-footer__link">Products</Link></li>
+        <div class="menu-footer__column _init">
+            <button type="button" 
+            className={spoilerActive ? buttonClassName + " _active" : buttonClassName} 
+            onClick={() => setSpoilerActive(!spoilerActive)}>Menu</button>
+            <ul className={spoilerActive ? contentClassName + " _slide-up" : contentClassName + " _slide-down"} ref={contentRef}>
+                <li><Link to="" class="menu-footer__link">Products</Link></li>
                 {/* <li><Link to="" class="menu-footer__link" activeClassName="">Rooms</Link></li>
                 <li><Link to="" class="menu-footer__link" activeClassName="">Inspirations</Link></li>
                 <li><Link to="" class="menu-footer__link" activeClassName="">About us</Link></li>
--- medClinic/mainapp-ui/src/componets/Spoiler.jsx
+++ medClinic/mainapp-ui/src/componets/Spoiler.jsx
@@ -1,7 +1,59 @@
-import {useCallback} from 'react';
+/* usage 
 
-const slideDown = (contentRef, duration = 500 ) =>{
-    if (contentRef != null){
+    import functions up and down:
+
+import {slideUp, slideDown} from pathToSpoiler;
+
+
+    define spoiler state:
+
+const [spoilerActive, setSpoilerActive] = useState(true);
+
+
+    define spoiler's button calssName and spoiler's content className:
+
+const buttonClassName = "menu-product__arrow _icon-arrow-down";
+const contentClassName = "menu-product__wrapper";
+
+
+    make refference to spoiler's content node 
+    and bind custom functions with up and down slide functions
+
+const contentRef = useRef(null);    
+const _slideDown = useCallback(slideDown, []);
+const _slideUp = useCallback(slideUp, []);
+    
+    call functions, when spoiler state has been changed
+useEffect(() => {
+    if (spoilerActive){
+        _slideDown(contentRef);
+    }
+    else{
+        _slideUp(contentRef);
+    }
+}, [spoilerActive]);
+
+
+exercise markup:
+
+<button data-spoller type="button" 
+className={spoilerActive ? buttonClassName + " _active" : buttonClassName}
+onClick={() => setSpoilerActive(!spoilerActive)}></button>
+<div className={spoilerActive ? contentClassName + " _slide-up" : contentClassName + " _slide-down"} ref={contentRef}>
+    <Description/>
+</div>
+
+when spoiler active, 
+spoiler's button  will add " _active" to className, 
+spoiler's content will add "_slide-up" or "-slide-down" to className
+don't forgot to add onClick handle to spoiler's button, which will set spoiler state value
+
+
+*/
+
+
+const slideDown = (contentRef, duration = 500) => {
+    if (contentRef != null) {
         if (contentRef.current.hidden) {
             contentRef.current.hidden = false;
         }
@@ -26,38 +78,33 @@ const slideDown = (contentRef, duration = 500 ) =>{
             contentRef.current.style.removeProperty('transition-property');
         }, duration);
     }
-    
+
 };
 
-const slideUp = (contentRef, duration = 500) =>{
-    if (contentRef != null){
+const slideUp = (contentRef, duration = 500) => {
+    if (contentRef != null) {
         contentRef.current.style.transitionProperty = 'height, margin, padding';
-    contentRef.current.style.transitionDuration = duration + 'ms';
-    contentRef.current.style.height = contentRef.current.offsetHeight + 'px';
-    contentRef.current.style.overflow = 'hidden';
-    contentRef.current.style.height = 0;
-    contentRef.current.style.paddingTop = 0;
-    contentRef.current.style.paddingBottom = 0;
-    contentRef.current.style.marginTop = 0;
-    contentRef.current.style.marginBottom = 0;
-    window.setTimeout(() => {
-        contentRef.current.hidden = true;
-        contentRef.current.style.removeProperty('height');
-        contentRef.current.style.removeProperty('padding-top');
-        contentRef.current.style.removeProperty('padding-bottom');
-        contentRef.current.style.removeProperty('margin-top');
-        contentRef.current.style.removeProperty('margin-bottom');
-        contentRef.current.style.removeProperty('overflow');
-        contentRef.current.style.removeProperty('transition-duration');
-        contentRef.current.style.removeProperty('transition-property');
-    }, duration);
+        contentRef.current.style.transitionDuration = duration + 'ms';
+        contentRef.current.style.height = contentRef.current.offsetHeight + 'px';
+        contentRef.current.style.overflow = 'hidden';
+        contentRef.current.style.height = 0;
+        contentRef.current.style.paddingTop = 0;
+        contentRef.current.style.paddingBottom = 0;
+        contentRef.current.style.marginTop = 0;
+        contentRef.current.style.marginBottom = 0;
+        window.setTimeout(() => {
+            contentRef.current.hidden = true;
+            contentRef.current.style.removeProperty('height');
+            contentRef.current.style.removeProperty('padding-top');
+            contentRef.current.style.removeProperty('padding-bottom');
+            contentRef.current.style.removeProperty('margin-top');
+            contentRef.current.style.removeProperty('margin-bottom');
+            contentRef.current.style.removeProperty('overflow');
+            contentRef.current.style.removeProperty('transition-duration');
+            contentRef.current.style.removeProperty('transition-property');
+        }, duration);
     }
-    
+
 };
 
-const Spoiler = () =>{
-    
-    
-}
-export default Spoiler
 export {slideDown, slideUp}
