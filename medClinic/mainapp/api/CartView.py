f47343d4421d06973732af0a23255954ea761eeb
master <hulktv222@mail.ru>
2021-07-24 12:50:39 +0300

    create api for cart

--- medClinic/mainapp/api/CartView.py
+++ medClinic/mainapp/api/CartView.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, response, status
 from rest_framework.decorators import action
 from .serializers.Cart import CartSerializer
@@ -17,7 +18,7 @@ class CartViewSet(viewsets.ModelViewSet):
     def get_cart(user):
         if user.is_authenticated:
             return Cart.objects.filter(owner=user.customer, for_anonymous_user=False).first()
-        return  Cart.objects.filter(for_anonymous_user=True).first()
+        return Cart.objects.filter(for_anonymous_user=True).first()
 
     @staticmethod
     def _get_or_create_cart_product(customer: Customer, cart: Cart, analyze: Analyze):
@@ -33,3 +34,34 @@ class CartViewSet(viewsets.ModelViewSet):
         cart = self.get_cart(self.request.user)
         cart_serializer = CartSerializer(cart)
         return response.Response(cart_serializer.data)
+
+    @action(methods=['put'], detail=False, url_path='current_customer_cart/product_add_to_cart/(?P<product_id>\d+)')
+    def product_add_to_cart(self, *args, **kwargs):
+        cart = self.get_cart(self.request.user)
+        analyze = get_object_or_404(Analyze, id=kwargs['product_id'])
+        cart_product, created = self._get_or_create_cart_product(self.request.user, cart, analyze)
+        if created:
+            cart.products.add(cart_product)
+            cart.save()
+            return response.Response({'detail': "Product successfully added to cart"})
+        return response.Response({'detail': "Product has already in cart"}, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(methods=['patch'], detail=False,
+            url_path='current_customer_cart/change_product_qty/(?P<qty>\d+)/(?P<cart_product_id>\d+)')
+    def change_product_qty(self, *args, **kwargs):
+        cart_product = get_object_or_404(CartAnalyze, id=kwargs['cart_product_id'])
+        cart_product.qty = int(kwargs['qty'])
+        cart_product.save()
+        cart_product.cart.save()
+        response.Response(status=status.HTTP_200_OK)
+
+    @action(methods=['delete'], detail=False,
+            url_path='current_customer_cart/product_remove_from_cart/(?P<cart_product_id>\d+)')
+    def product_remove_from_cart(self, *args, **kwargs):
+        cart = self.response.user
+        cart_product = get_object_or_404(CartAnalyze, id=kwargs['cart_product_id'])
+        cart.products.remove(cart_product)
+        cart_product.delete()
+        cart.save()
+        return response.Response(status=status.HTTP_204_NO_CONTENT)
+
