dc38d329bfb794e56c412c99289ada86c925ed4c
oleg <hulktv222@mail.ru>
2021-08-11 16:18:50 +0300

    markup empty cart page

--- medClinic/mainapp-ui/src/componets/Cart/CartContainer.jsx
+++ medClinic/mainapp-ui/src/componets/Cart/CartContainer.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { postApiRequest } from '../../api_requests';
 import { setIsAuthAC, setIsLoadingAC, setIsNeedRedirectAC, setUserDataAC } from '../../redux/auth-reducer';
 import { Redirect } from 'react-router-dom';
+import './CartPage.scss';
 
 const Cart = (props) =>{
 
@@ -14,19 +15,30 @@ const Cart = (props) =>{
     if (props.cart.total_products === 0){
         return (
             <main className="page">
-            <section className="page__notFound notFound">
-                <div className="notFound__container _container">
-                    <div className="notFound__content">
-                     Loading...
+                <section className="page__base base-block">
+                    <div className="base-block__container _container">
+                        <div className="base-block__content empty-cart">
+                            <div className="empty-cart__left-part left-part">
+                                <h3 className="left-part__title _title">Ваша корзина пуста</h3>
+                                <div className="left-part__text">
+                                    <p>
+                                    Вы еще не добавили ни одного продукта. Раздел «Анализы» поможет вам найти необходимое исследование.
+                                    </p>
+                                </div>
+                                <button className="left-part__button btn _icon-arrow-link">ПЕРЕЙТИ В КАТАЛОГ</button>
+                            </div>
+                            <div className="empty-cart__right-part right-part">
+                                <button className="right-part__cart-icon _icon-cart"></button>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </section>
-        </main>
+                </section>
+            </main>
         )
     }
     return (
         <main className="page">
-            <section className="page__notFound notFound">
+            <section className="page__base notFound">
                 <div className="notFound__container _container">
                     <div className="notFound__content">
                     </div>
--- medClinic/mainapp-ui/src/componets/InWork/InWork.jsx
+++ medClinic/mainapp-ui/src/componets/InWork/InWork.jsx
@@ -4,9 +4,9 @@ import './InWork.scss'
 const InWork = () => {
     return (
         <main className="page">
-            <section className="page__notFound notFound">
-                <div className="notFound__container _container">
-                    <div className="notFound__content">
+            <section className="page__base base-block">
+                <div className="base-block__container _container">
+                    <div className="base-block__content notFound">
                         <div className="notFound__icon-warning"></div>
                         <h2 className="notFound__title _title">Что-то пошло не так и мы не можем загрузить страницу по этой ссылке...</h2>
                         <div className="notFound__text">Наш сайт находится в разработке и, возможно, в скором времени на этой странице появится контент. В любом случае проверьте ссылку на правильнность и вернитесь через некоторое время</div>
