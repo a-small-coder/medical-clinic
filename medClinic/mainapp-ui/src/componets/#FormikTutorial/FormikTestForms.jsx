6da1237069721483a42c8cdf35c4e0ea2523b011
oleg <hulktv222@mail.ru>
2021-08-15 10:52:30 +0300

    adding material-ui styling for forms

--- medClinic/mainapp-ui/src/componets/#FormikTutorial/FormikTestForms.jsx
+++ medClinic/mainapp-ui/src/componets/#FormikTutorial/FormikTestForms.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Field, Form, Formik} from 'formik';
-import {Card, CardContent, Typography} from '@material-ui/core';
+import {Field, Form, Formik, useField} from 'formik';
+import {Card, CardContent, Checkbox, FormControlLabel, MenuItem, TextField, Typography} from '@material-ui/core';
 
 function FormikTestForms(props) {
     const initialValues = {
@@ -20,14 +20,30 @@ function FormikTestForms(props) {
                 {
                     ({values}) => (
                         <Form>
-                            <Field name="fullname"/>
-                            <Field name="initialInvestment" type="number"/>
+                            <Field name="fullname" as={TextField} lavel="Full Name"/>
+                            <Field 
+                                name="initialInvestment" 
+                                type="number" 
+                                as={TextField} 
+                                lavel="InitialInvestment "
+                            />
 
-                            <Field name="investmentRisk" value="Higth" type="checkbox"/>
-                            <Field name="investmentRisk" value="Medium" type="checkbox"/>
-                            <Field name="investmentRisk" value="Low" type="checkbox"/>
+                            <MyCheckbox name="investmentRisk" value="Higth" label="High - Very Risky"/>
+                            <MyCheckbox name="investmentRisk" value="Medium" label="Medium - Risky"/>
+                            <MyCheckbox name="investmentRisk" value="Low" label="Low - Safe"/>
 
-                            <Field name="commentAboutInvestmentRisk" as="textarea"/>
+                            <Field name="commentAboutInvestmentRisk" as={TextField} multiline rows={5}/>
+
+                            <Field name="dependents" as={TextField} select>
+                                <MenuItem value={0}>0</MenuItem>
+                                <MenuItem value={1}>1</MenuItem>
+                                <MenuItem value={2}>2</MenuItem>
+                                <MenuItem value={3}>3</MenuItem>
+                                <MenuItem value={4}>4</MenuItem>
+                                <MenuItem value={5}>5</MenuItem>
+                            </Field>
+
+                            <MyCheckbox name="acceptedTermsAndConditions" label="Accepted terms and conditions"/>
 
                             <pre>{JSON.stringify(values, null, 4)}</pre>
                         </Form>
@@ -40,3 +56,12 @@ function FormikTestForms(props) {
 }
 
 export default FormikTestForms;
+
+export function MyCheckbox(props) {
+    const [field] = useField({
+        name: props.name,
+        type: 'checkbox',
+        value: props.value
+    })
+    return <FormControlLabel control={<Checkbox {...props} {...field} />} label={props.label} />
+}
