88277a6dc79b77c87855ce5e692bd9250e5cc604
oleg <hulktv222@mail.ru>
2021-08-15 14:26:47 +0300

    create fields for MultilineForm and make a FormikStep and FormikStepper components

--- medClinic/mainapp-ui/src/componets/#FormikTutorial/MultiStepForm.jsx
+++ medClinic/mainapp-ui/src/componets/#FormikTutorial/MultiStepForm.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {ErrorMessage, Field, Form, Formik, useField} from 'formik';
-import {Box, Card, CardContent, Checkbox, FormControlLabel, FormGroup, TextField, Typography} from '@material-ui/core';
+import {Box, Button, Card, CardContent, Checkbox, FormControlLabel, FormGroup, TextField, Typography, } from '@material-ui/core';
 import * as Yup from 'yup';
+import {CheckboxWithLabel} from 'formik-material-ui';
 function MultiStepForm(props) {
     const initialValues = {
         firstName: '',
@@ -11,9 +12,23 @@ function MultiStepForm(props) {
         description: '',
     };
 
-    const validationSchema = Yup.object({
+    const validationSchemaFirstStep = Yup.object({
+        money: Yup.mixed().when('millionaire', {
+            is: true,
+            then: Yup.number().required().min(1_000_000),
+            otherwise: Yup.number().required()
+        }),
+    })
+    const validationSchemaSecondStep = Yup.object({
         firstName: Yup.string().required().min(2).max(50),
         lastName: Yup.string().required().min(2).max(50),
+        money: Yup.mixed().when('millionaire', {
+            is: true,
+            then: Yup.number().required().min(1_000_000),
+            otherwise: Yup.number().required()
+        }),
+    })
+    const validationSchemaThirdStep = Yup.object({
     })
     const sumbmitHandler = (values, formikHelpers) =>{
         return new Promise(res => {
@@ -30,12 +45,12 @@ function MultiStepForm(props) {
             <CardContent>
                 <Typography variant="h4">New Account</Typography>
             
-                <Formik 
-                validationSchema={validationSchema}
-                initialValues={initialValues} onSubmit={sumbmitHandler}>
-                {
-                    ({values, errors, isSubmitting, isValidating}) => (
-                        <Form>
+                <FormikStepper
+                initialValues={initialValues}
+                onSubmit={sumbmitHandler}
+                >
+                    <FormikStep >
+                        <Box marginBottom={10}>
                             <Box marginBottom={2}>
                                 <FormGroup>
                                     <Field name="firstName" as={TextField} label="Full Name" />
@@ -44,26 +59,44 @@ function MultiStepForm(props) {
                             </Box>
                             <Box marginBottom={2}>
                                 <FormGroup>
-                                    <Field name="lastName" as={TextField} label="Second Name" />
+                                    <Field name="lastName" as={TextField} label="Last Name" />
                                     <ErrorMessage name="lastName" />
                                 </FormGroup>
                             </Box>
-                            <button
-                                className="button-block__button btn _filled-btn _green"
-                                type='submit'
-                                disabled={isSubmitting || isValidating}
-                            >
-                                ОТПРАВИТЬ
-                            </button>
                             <Box marginBottom={2}>
-                                <pre>{JSON.stringify(errors, null, 4)}</pre>
-                                <pre>{JSON.stringify(values, null, 4)}</pre>
+                                <FormGroup>
+                                    <Field
+                                        name="millionaire"
+                                        type="checkbox"
+                                        component={CheckboxWithLabel}
+                                        Label={{ label: "I'm a millionaire" }}
+                                    />
+                                    <ErrorMessage name="millionaire" />
+                                </FormGroup>
                             </Box>
-                            
-                        </Form>
-                    )
-                }
-            </Formik>
+                        </Box>
+                    </FormikStep>
+                    <FormikStep validationSchema={validationSchemaFirstStep}>
+                        <Box marginBottom={10}>
+                            <Box marginBottom={2}>
+                                <FormGroup>
+                                    <Field name="money" as={TextField} label="How much you have?" />
+                                    <ErrorMessage name="money" />
+                                </FormGroup>
+                            </Box>
+                        </Box>
+                    </FormikStep>
+                    <FormikStep>
+                        <Box marginBottom={10}>
+                            <Box marginBottom={2}>
+                                <FormGroup>
+                                    <Field name="description" as={TextField} label="description" />
+                                    <ErrorMessage name="description" />
+                                </FormGroup>
+                            </Box>
+                        </Box>
+                    </FormikStep>
+            </FormikStepper>
             </CardContent>
         </Card>
 
@@ -78,4 +111,42 @@ export function MyCheckbox(props) {
         value: props.value,
     })
     return <FormControlLabel control={<Checkbox {...props} {...field} />} label={props.label} />
+}
+
+export function FormikStep(props){
+    return <>{props.children}</>
+}
+
+export function FormikStepper(props) {
+    const childrenArray = React.Children.toArray(props.children)
+    const [step, setStep] = useState(0)
+    const currentChild = childrenArray[step]
+
+    function isLastStep(){
+        return step === childrenArray.length -1;
+    }
+
+    return (
+        <Formik {...props} 
+        validationSchema={currentChild.props.validationSchema}
+        onSubmit={async (values, helpers) => {
+            if (isLastStep()){
+                await props.onSubmit(values, helpers);
+            }
+            else{
+                setStep(s=> s+1);
+            }
+        }}
+        >
+            <Form autoComplete="off">
+            {currentChild}
+
+                {step > 0 ?
+                    <Button onClick={() => setStep(s => s - 1)}>Назад</Button> :
+                    null
+                }
+                <Button type="submit">{isLastStep() ? "Отправить" : "Далее"}</Button>
+            </Form>
+        </Formik>
+    );
 }
