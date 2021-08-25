610490e03b5ad3fd3abd61f36efcd6c124b02235
oleg <hulktv222@mail.ru>
2021-08-25 17:05:59 +0300

    new api for new models

--- medClinic/mainapp/api/AnalyzeView.py
+++ medClinic/mainapp/api/AnalyzeView.py
@@ -1,52 +1,81 @@
-# from rest_framework import viewsets, response, status, filters
-#
-# from .OtherViews import CatalogPagination
-# from .serializers.Analyzes import AnalyzeSerializer, AnalyzeListSerializer, AnalyzeRetrieveSerializer, \
-#     AnalyzeContentBlockSerializer
-# from ..models import (
-#     Analyze,
-#     AnalyzeContentBlock
-# )
-#
-#
-# class AnalyseViewSet(viewsets.ModelViewSet):
-#
-#     queryset = Analyze.objects.all().order_by('id')
-#     serializer_class = AnalyzeSerializer
-#     pagination_class = CatalogPagination
-#
-#     action_to_serializer = {
-#         "list": AnalyzeListSerializer,
-#         "retrieve": AnalyzeRetrieveSerializer
-#     }
-#
-#     def get_serializer_class(self):
-#         return self.action_to_serializer.get(
-#             self.action,
-#             self.serializer_class
-#         )
-#
-#
-# class UnicAnalyseViewSet(viewsets.ModelViewSet):
-#
-#     queryset = Analyze.objects.filter(is_unic=True)
-#     serializer_class = AnalyzeSerializer
-#     pagination_class = CatalogPagination
-#
-#     action_to_serializer = {
-#         "list": AnalyzeSerializer,
-#         "retrieve": AnalyzeRetrieveSerializer
-#     }
-#
-#     def get_serializer_class(self):
-#         return self.action_to_serializer.get(
-#             self.action,
-#             self.serializer_class
-#         )
-#
-#
-#
-# class AnalyzeContentBlockViewSet(viewsets.ModelViewSet):
-#
-#     queryset = AnalyzeContentBlock.objects.all()
-#     serializer_class = AnalyzeContentBlockSerializer
+from rest_framework import viewsets, response, status, filters
+
+from .OtherViews import CatalogPagination
+from .serializers.Analyzes import *
+from ..models import *
+
+
+class ProductsView(viewsets.ModelViewSet):
+
+    queryset = Product.objects.order_by('price')
+    serializer_class = ProductSerializer
+    pagination_class = CatalogPagination
+
+
+class ComplexesView(viewsets.ModelViewSet):
+    queryset = AnalyzeComplex.objects.order_by('price')
+    serializer_class = AnalyzeComplexRetrieveSerializer
+    pagination_class = CatalogPagination
+
+    action_to_serializer = {
+        "list": ProductSerializer,
+        "retrieve": AnalyzeComplexRetrieveSerializer
+    }
+
+    def get_serializer_class(self):
+        print(self.action)
+        return self.action_to_serializer.get(
+            self.action,
+            self.serializer_class
+        )
+
+
+class ComplexAnalyzesTopServicesViewSet(viewsets.ModelViewSet):
+    queryset = AnalyzeComplex.objects.filter(on_main_page=True)
+    serializer_class = AnalyzeComplexTopServicesSerializer
+
+
+class ComplexAnalyzesTopFiveViewSet(viewsets.ModelViewSet):
+    queryset = AnalyzeComplex.objects.filter(in_top_five_list=True)
+    serializer_class = AnalyzeComplexTopServicesSerializer
+
+
+class AnalyzesView(viewsets.ModelViewSet):
+    queryset = Analyze.objects.order_by('price')
+    serializer_class = AnalyzeRetrieveSerializer
+    pagination_class = CatalogPagination
+
+    action_to_serializer = {
+        "list": ProductSerializer,
+        "retrieve": AnalyzeRetrieveSerializer
+    }
+
+    def get_serializer_class(self):
+        return self.action_to_serializer.get(
+            self.action,
+            self.serializer_class
+        )
+
+
+class UnicAnalyseViewSet(viewsets.ModelViewSet):
+
+    queryset = Analyze.objects.filter(is_unic=True)
+    serializer_class = AnalyzeRetrieveSerializer
+    pagination_class = CatalogPagination
+
+    action_to_serializer = {
+        "list": ProductSerializer,
+        "retrieve": AnalyzeRetrieveSerializer
+    }
+
+    def get_serializer_class(self):
+        return self.action_to_serializer.get(
+            self.action,
+            self.serializer_class
+        )
+
+
+class AnalyzeContentBlockViewSet(viewsets.ModelViewSet):
+
+    queryset = AnalyzeContentBlock.objects.all()
+    serializer_class = AnalyzeContentBlockSerializer
--- medClinic/mainapp/api/urls.py
+++ medClinic/mainapp/api/urls.py
@@ -1,37 +1,23 @@
 from django.urls import path
 from rest_framework import routers
 
-# from .AnalyzeView import (
-    # AnalyseViewSet,
-    # UnicAnalyseViewSet,
-    # AnalyzeContentBlockViewSet,
-# )
-# from .AnalyzesComplexView import (
-#     ComplexAnalyzesViewSet,
-#     ComplexAnalyzesTopServicesViewSet,
-#     ComplexAnalyzesTopFiveViewSet,
-# )
-# from .CartView import CartViewSet
-from .OtherViews import (
-    NavigationCategoryViewSet,
-    SubNavigationCategoryViewSet,
-    AboutUsCategoryViewSet,
-    OurAchievementsViewSet,
-    UserViewSet,
-    AuthViewSet,
-)
+from .AnalyzeView import *
+from .OtherViews import *
+from .CartView import *
+from .AnalyzesComplexView import *
 
 router = routers.SimpleRouter()
 router.register('navigation', NavigationCategoryViewSet, basename='navigation')
 router.register('subNavigation', SubNavigationCategoryViewSet, basename='subNavigation')
-# router.register('best-products', ComplexAnalyzesTopServicesViewSet, basename='best-products')
-# router.register('best-complex-analyzes', ComplexAnalyzesTopFiveViewSet, basename='best-complex-analyzes')
+router.register('best-products', ComplexAnalyzesTopServicesViewSet, basename='best-products')
+router.register('best-complex-analyzes', ComplexAnalyzesTopFiveViewSet, basename='best-complex-analyzes')
 # router.register('stocks', , basename='stocks')
 router.register('about-us', AboutUsCategoryViewSet, basename='about-us')
-# router.register('catalog/all-analyzes', AnalyseViewSet, basename='all-analyzes')
-# router.register('catalog/unic-analyzes', UnicAnalyseViewSet, basename='unic-analyzes')
-# router.register('catalog/complex-analyzes', ComplexAnalyzesViewSet, basename='all-analyzes')
-# router.register('analyze-content', AnalyzeContentBlockViewSet, basename='analyze-content-block')
+router.register('catalog/all', ProductsView, basename='all-products')
+router.register('catalog/all-analyzes', AnalyzesView, basename='all-analyzes')
+router.register('catalog/unic-analyzes', UnicAnalyseViewSet, basename='unic-analyzes')
+router.register('catalog/complex-analyzes', ComplexesView, basename='all-complexes')
+router.register('analyze-content', AnalyzeContentBlockViewSet, basename='analyze-content-block')
 # router.register('cart', CartViewSet, basename='cart')
 router.register('achievements', OurAchievementsViewSet, basename='achievements')
 router.register('auth/users', UserViewSet, basename='users')
